69078553162f1f96ed5a28ed136513fad1782030
Mariusz Nowak <medyk@medikoo.com>
2015-09-05 20:07:56 +0200

    isDirShadowed utility

---
+++ test/lib/is-dir-shadowed.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var deferred = require('deferred')
+  , resolve  = require('path').resolve
+
+  , pgPath = resolve(__dirname, '../__playground');
+
+module.exports = function (t, a, d) {
+	return deferred(
+		t(resolve(pgPath, 'test-package/inner'), '')(function (result) {
+			a(result, false, "Dir with index");
+		}),
+		t(resolve(pgPath, 'habla'), '.js')(function (result) {
+			a(result, false, "Non existing dir");
+		}),
+		t(resolve(pgPath, 'test'), '.json')(function (result) {
+			a(result, true, "Shadowed dir");
+		})
+	).done(function () { d(); }, d);
+};
